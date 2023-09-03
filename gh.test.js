4a67a62e5e0af69bf99bc1b5efc503568c69b9cc
Tatyana Larina <jerrymurr@ya.ru>
2023-09-04 00:22:19 +0300

    first commit

---
+++ gh.test.js
@@ -0,0 +1,60 @@
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+  }, 6000);
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  }, 6000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toMatch("Get started with Team")
+  }, 6000);
+});
+
+describe("2 Title tests", () => {
+
+  test("About", async () => {
+    await page.goto("https://github.com/about");
+    const title = await page.title();
+    expect(title).toEqual("About · GitHub");
+  }, 6000);
+
+  test("Security", async () => {
+    await page.goto("https://github.com/security");
+    const title = await page.title();
+    expect(title).toEqual("GitHub Security · GitHub");
+  }, 6000);
+
+  test("Partners", async () => {
+    await page.goto("https://partner.github.com/");
+    const title = await page.title();
+    expect(title).toEqual("Partner with GitHub | GitHub Partner Portal");
+  }, 6000);
+
+});
+
